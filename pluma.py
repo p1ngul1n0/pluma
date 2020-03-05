282898ec5d532e7f04152026b8e54eee8ce18975
sampzzz <57546870+sampzzz@users.noreply.github.com>
2020-03-05 12:10:55 -0300

    Add files via upload

---
+++ pluma.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from web import *
+from spider import *
+from datetime import datetime
+import time
+from pyfiglet import Figlet
+
+try:
+	f = Figlet(font='roman')
+	print (f.renderText('Pluma'))
+	print ("|-Versão: 0.3")
+	print ("|-Autor: Lucas Antoniaci")  
+	print ("_____________________________________________________________________________________________________________________________")
+	url = url(input(" > Insira a URL para análise: "))
+	if url.verificaUrl():
+		now = datetime.now()
+		inicio = time.time()
+		print (" > DATA {}".format(now.strftime("%d/%m/%Y %H:%M:%S")))
+		scan = scan(url)
+		scan.info()
+		scan.heads()
+		scan.cookies()
+		scan.methods()
+		scan.autocomplete()
+		scan.enum()
+		spider = spider(url.url)
+		spider.extrair(spider.url_master)
+		i = 1
+		for link in spider.links_internos:
+			spider.extrair(link)
+			i += 1
+		fim = time.time()
+		print ("\n\nAnd...Done! :) [{} secs]".format(int(fim - inicio)))
+except (KeyboardInterrupt, SystemExit):
+        print ("\nEXECUÇÃO INTERROMPIDA!")
